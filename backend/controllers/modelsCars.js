af43a4e64f6d47f800cf761a8525024e2f81aac6
Hervé Delettre <rv.delettre@gmail.com>
2024-11-11 21:44:44 +0100

    init frontend

--- backend/controllers/modelsCars.js
+++ backend/controllers/modelsCars.js
@@ -2,7 +2,21 @@ const MODELCARS = require("../models/modelsCars");
 
 exports.createModelCar = async (req, res) => {};
 
-exports.getAllModelCars = async (req, res) => {};
+exports.getAllModelCars = async (req, res) => {
+  try {
+    const response = await MODELCARS.findAll();
+    if (!response) {
+      return res
+        .status(404)
+        .json({ message: "Aucune donnée n'a été trouvée !" });
+    }
+    return res.status(200).json(response);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Le serveur est indisponible !", error: error });
+  }
+};
 
 exports.getOneModelCar = async (req, res) => {};
 
---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="appContainer">
+      APP
+    </div>
+  );
+}
+
+export default App;
